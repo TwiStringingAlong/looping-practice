5f10adc4c3c700433e2a62ee8fcf5bf0c3eaa8b9
TwiStringingAlong <121710664+TwiStringingAlong@users.noreply.github.com>
2023-06-20 03:09:13 -0400

    Update basketball-score.js

--- basketball-score/basketball-score.js
+++ basketball-score/basketball-score.js
@@ -1,3 +1,17 @@
 let buckets = [ "fg", "tp", "ft", "ft", "tp", "tp", "tp", "fg", "fg", "tp", "fg", "tp", "ft", "ft", "ft", "tp" ]
 
 // Write code here
+let buckets = [ "fg", "fg", "ft", "tp" ]
+let fg = "2"
+    console.log(fg)
+let ft = "1"
+    console.log(ft)
+let tp = "3"
+    console.log(tp)
+console.log(buckets)
+
+let sum = 0;
+for (let i = 0; i < buckets.length; i++) {
+  sum += buckets[i]
+}
+console.log(sum)
