c0c16db24ffd5395f60714fb5190c6263352c3b6
TwiStringingAlong <121710664+TwiStringingAlong@users.noreply.github.com>
2023-06-21 19:13:40 -0400

    Update basketball-score.js

--- basketball-score/basketball-score.js
+++ basketball-score/basketball-score.js
@@ -1,3 +1,17 @@
 let buckets = [ "fg", "tp", "ft", "ft", "tp", "tp", "tp", "fg", "fg", "tp", "fg", "tp", "ft", "ft", "ft", "tp" ]
 
 // Write code here
+let buckets = [ "fg", "fg", "ft", "tp" ]
+
+
+let sum = 0;
+for (let i = 0; i < buckets.length; i++) {
+    if (buckets[i] == "fg"){
+      sum = sum + 2
+} else if (buckets[i] == "ft"){
+        sum += 1
+} else if (buckets[i] == "tp"){
+        sum += 3
+  }
+}
+console.log(sum)
