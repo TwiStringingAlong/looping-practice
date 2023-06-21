db4b8c9d2a4531105fd141882d7447a1c34ed7dc
TwiStringingAlong <121710664+TwiStringingAlong@users.noreply.github.com>
2023-06-21 01:59:07 -0400

    Update vertical-string.js

--- vertical-string/vertical-string.js
+++ vertical-string/vertical-string.js
@@ -1,3 +1,12 @@
 let myWord = "Pneumonoultramicroscopicsilicovolcanoconiosis"
 
 // Write your code here
+//WILL REVISIT I THINK ITS CORRECT BUT MAYBE TOO LONG OF A SOLUTION
+
+let text = "Pneumonoultramicroscopicsilicovolcanoconiosis";
+const myArray = text.split("");
+    console.log(myArray)
+    
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i])
+}
