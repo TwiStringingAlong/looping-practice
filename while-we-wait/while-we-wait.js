1655b0105cbbf3003c114d340de4594f1a9ae84e
TwiStringingAlong <121710664+TwiStringingAlong@users.noreply.github.com>
2023-06-21 03:43:27 -0400

    Update while-we-wait.js

--- while-we-wait/while-we-wait.js
+++ while-we-wait/while-we-wait.js
@@ -2,3 +2,15 @@ let myArray = [ "wait", "wait", "wait","wait", "wait", "wait","wait", "wait", "w
 
 
 // Write code here
+//THIS IS INCORRECT I NEED TO REVISIT
+let myArray = [ "wait", "wait", "wait","wait", "wait", "wait","wait", "wait", "wait","wait", "wait", "wait", "STOP","wait", "wait", "wait","wait", "wait", "wait","wait", "wait", "wait","wait", "wait", "wait" ]
+
+var count = 0;
+var number = "wait";
+
+for (var i = 0; i < myArray.length; i++){
+    if (myArray[i] == number) {
+        count++;
+    }
+}
+console.log(count);
